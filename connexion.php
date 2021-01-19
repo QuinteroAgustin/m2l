445635489cee277bc0faade62ae5a1d995e2e023
H-ile <achille.monteil@limayrac.fr>
2021-01-19 16:34:43 +0100

    ajout boutton

--- connexion.php
+++ connexion.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Page de connexion</title>
 </head>
 <body>
+<H1>Se connecter</H1>
+
 <div class="mb-3 row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
     <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+      <input type="text" class="form-control-plaintext" id="inputEmail" value="email@example.com">
     </div>
   </div>
   <div class="mb-3 row">
@@ -17,6 +19,9 @@
     <div class="col-sm-10">
       <input type="password" class="form-control" id="inputPassword">
     </div>
+    <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Se connecter</button>
+  </div>
   </div>
 </body>
 </html>
