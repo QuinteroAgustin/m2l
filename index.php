c9f873a4923c2bc95e12f99692fa250f6a86b1f6
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-01-19 15:58:01 +0100

    Creation index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M2L</title>
+</head>
+<body>
+    <h1>Maison de ligue</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cum blanditiis, provident exercitationem vero explicabo in eos dolores cumque iste, magni ad optio non harum mollitia laboriosam officia! Molestias, autem.</p>
+</body>
+</html>
