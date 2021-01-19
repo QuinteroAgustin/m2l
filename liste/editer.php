eb1627d727543d59ed906ceffb8fc82a79d05897
DavidPeyrard <david.peyrard@limayrac.fr>
2021-01-19 17:09:00 +0100

    ok

---
+++ liste/editer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M2L - Modifier</title>
+</head>
+<body>
+    <h1>Modifier une question / Répondre</h1>
+
+    <h3>Question</h3>
+
+    <textarea name="question" id="question" rows="10" cols="50" >Est ce que le ballon est rond ?</textarea>
+
+    <h3>Réponse</h3>
+
+    <textarea name="reponse" id="reponse" rows="10" cols="50" >Bien sûr que oui</textarea>
+
+
+</body>
+</html>
