a9a8c0a4fc8bb7c0295e3c1dd387487de4d72335
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-01-19 17:18:09 +0100

    ajout page déconnexion

---
+++ liste/deconnexion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Maison des Ligues</h1>
+<h2>Déconnexion</h2>
+</br>
+Vous êtes bien déconnecté !
+
+<a href="index.php">Retour à la page d'accueil</a>
+</body>
+</html>
