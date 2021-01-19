74b2c226f2b6c054c5560291b2a0b8e56c6f6c50
DavidPeyrard <david.peyrard@limayrac.fr>
2021-01-19 16:36:02 +0100

    modif liste

---
+++ liste/liste.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M2L - Liste</title>
+</head>
+<body>
+    <h1>Liste des questions</h1>
+
+    <table>
+        <tr>
+            <th>Nr</th>
+            <th>Auteur</th>
+            <th>Question</th>
+            <th>Réponse</th>
+            <th>Actions</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Dav</td>
+            <td>Est ce que le ballon est rond ?</td>
+            <td>Bien sûr que oui</td>
+            <td><a href="editer.php">Modifier</a></td>
+            <td><a href="supprimer.php">Supprimer</a></td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Gus</td>
+            <td>Que signifie PSG ?</td>
+            <td>Pas sûr de gagner</td>
+            <td><a href="editer.php">Modifier</a></td>
+            <td><a href="supprimer.php">Supprimer</a></td>
+        </tr>
+
+        <a href="ajouter.php">Ajouter une question</a>
+
+    </table>
+</body>
+</html>
