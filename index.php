bed5f7a62e998e68374eb5046e6418fbbf8a1248
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-01-19 16:59:04 +0100

    ajout css sur la nav bar

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>M2L</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <nav>
+    <nav class="navbar bg-primary">
     <h1>Maison de ligue</h1>
     <a href="liste/liste.php">FAQ</a>
     <br>
