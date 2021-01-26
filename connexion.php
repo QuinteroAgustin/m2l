b7d6b617552599b3f689c1671370ca87126e0d08
H-ile <achille.monteil@limayrac.fr>
2021-01-26 13:35:28 +0100

    jsp

--- connexion.php
+++ connexion.php
@@ -4,28 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de connexion</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <h1>M2L</h1>
 <H2>Se connecter</H2>
-
-<div class="mb-3 row">
-    <label for="inputpseudo" class="col-sm-2 col-form-label">Pseudo</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control-plaintext" id="inputpseudo">
-    </div>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nom d'utilisateur</label>
+    <div id="emailHelp" class="form-text">Ne pas divulgué sont pseudo</div>
   </div>
-  <div class="mb-3 row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword">
-    </div>
-    <br>
-    <div class="col-auto">
-    <button type="submit" class="btn btn-primary mb-3">Ok</button>
-    <button type="annulation" class="btn btn-primary mb-3">Annuler</button>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <a href="index.php">acceuil</a>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </body>
 </html>
