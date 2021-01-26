b6ef88a4fbac8d95f10536ef3b448636551a50ac
DavidPeyrard <david.peyrard@limayrac.fr>
2021-01-26 14:15:46 +0100

    affichage propre

--- inscription.php
+++ inscription.php
@@ -20,16 +20,16 @@
         <form action="index.php" method="POST">
             <label for="pseudo">Pseudo : </label>
             <input type="text" id="pseudo" name="pseudo">
-            <br>
+            <br><br>
             <label for="email">Email : </label>
             <input type="text" id="email" name="email">
-            <br>
+            <br><br>
             <label for="password">Mot de passe : </label>
             <input type="password" id="password" name="password">
-            <br>
+            <br><br>
             <label for="password2">Confirmer le mot de passe : </label>
             <input type="password" id="password2" name="password2">
-            <br>
+            <br><br>
             <label for="ligue">Ligue : </label>
             <select name="ligue" id="ligue">
                 <option value="l1" selected="selected">Ligue de basket</option>
@@ -37,8 +37,8 @@
                 <option value="l1">Ligue de handball</option>
                 <option value="l1">Ligue de football</option>
             </select>
-            <br>
-            <input type="submit" value="S'inscricre">
+            <br><br>
+            <input type="submit" value="S'inscrire">
         </form>
     </div>
 </body>
