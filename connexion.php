3a96762ee3b58b9596d01ecd0502e3f60ad59559
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-01-26 14:02:23 +0100

    page de connexion a jour

--- connexion.php
+++ connexion.php
@@ -3,26 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page de connexion</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>M2L - Connexion</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-<h1>M2L</h1>
-<H2>Se connecter</H2>
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Nom d'utilisateur</label>
-    <div id="emailHelp" class="form-text">Ne pas divulguer son pseudo</div>
+  <div>
+    <ul class="navbar">
+      <li class="ligne left"><a class="tjaune"href="index.php">Accueil</a></li>
+      <li class="ligne left"><a href="liste/liste.php">FAQ</a></li>
+      <li class="ligne right"><a href="inscription.php">Inscription</a></li>
+      <li class="ligne right"><a class="active" href="connexion.php">Connexion</a></li>
+    </ul>
   </div>
-  <div class="mb-3 row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword">
-    </div>
-    <br>
-    <div class="col-auto">
-    <form action="index.php"><button type="submit" class="btn btn-primary">Se connecter</button></form>
-    <br><br>
-    <form action="index.php"><button type="annulation" class="btn btn-primary">Annuler</button></form>
+  <div class="marge">
+    <h1>Se connecter</h1>
+    <form action="index.php" methode="POST">
+      <label for="pseudo">Pseudo</label>
+      <input type="text" name="pseudo" id="pseudo">
+      <br>
+      <label for="password">Mot de passe</label>
+      <input type="password" name="password" id="password">
+      <br>
+      <input type="submit" value="Se connecter">
+    </form>
+  </div>
 </body>
 </html>
