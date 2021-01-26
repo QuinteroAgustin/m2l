cc9a8faf03e9959a766f15d5de956c0c48fffbd8
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-01-26 13:49:46 +0100

    navbar fini

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
 </head>
 <body>
     <div>
-        <ul>
-            <li class="ligne left"><a href="default.asp">Accueil</a></li>
+        <ul class="navbar">
+            <li class="ligne left"><a class="tjaune active"href="index.php">Accueil</a></li>
             <li class="ligne left"><a href="liste/liste.php">FAQ</a></li>
             <li class="ligne right"><a href="inscription.php">Inscription</a></li>
             <li class="ligne right"><a href="connexion.php">Connexion</a></li>
         </ul>
     </div>
-    <div>
-    <h1>Maison de ligue</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in illo dolorum eveniet unde explicabo aut incidunt deserunt nemo, qui nesciunt officiis modi, sint laboriosam numquam ut dolorem sit voluptas.lore Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam qui nulla natus reprehenderit doloribus nihil corporis architecto est voluptatem expedita mollitia recusandae voluptates asperiores enim, voluptatum inventore neque voluptatibus. Autem?</p>
+    <div class="marge">
+        <h1>Maison de ligue</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in illo dolorum eveniet unde explicabo aut incidunt deserunt nemo, qui nesciunt officiis modi, sint laboriosam numquam ut dolorem sit voluptas.lore Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam qui nulla natus reprehenderit doloribus nihil corporis architecto est voluptatem expedita mollitia recusandae voluptates asperiores enim, voluptatum inventore neque voluptatibus. Autem?</p>
     </div>
-    </body>
+</body>
 </html>
