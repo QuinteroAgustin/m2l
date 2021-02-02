2dfa6ff2005e69b691cc523b38fa0e5cbf69eb45
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-02-02 16:48:37 +0100

    footer et header

---
+++ footer.php
@@ -0,0 +1,9 @@
+    </div>
+    <footer>
+        <div class="footer">
+        <ul class="foot_left">INFOS PRATIQUES</ul>
+        <ul class="foot_right">CONTACT</ul>
+        </div>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,36 +1,11 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>M2L</title>
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="icon" href="img/logo.png">
-</head>
-<body>
-    <div>
-        <ul class="navbar">
-            <li class="ligne left"><a class="tjaune active"href="index.php">Accueil</a></li>
-            <li class="ligne left"><a href="liste/liste.php">FAQ</a></li>
-            <li class="ligne right"><a href="inscription.php">Inscription</a></li>
-            <li class="ligne right"><a href="connexion.php">Connexion</a></li>
-        </ul>
-    </div>
-    <div class="marge">
-        <br><br>
-    <div id="bloc_page">
-        <header>
-            <img src="img/logo.png" alt="Logo_page" title="Accueil" id="logo" class="centerpng"/>
+<?php 
+    $title = "Accueil";
+    require('header.php'); 
+?>
+    <br>
+    <img src="img/logo.png" alt="logo">
     <br>
         <h1>Maison de ligue</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in illo dolorum eveniet unde explicabo aut incidunt deserunt nemo, qui nesciunt officiis modi, sint laboriosam numquam ut dolorem sit voluptas.lore Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam qui nulla natus reprehenderit doloribus nihil corporis architecto est voluptatem expedita mollitia recusandae voluptates asperiores enim, voluptatum inventore neque voluptatibus. Autem?</p>
     </div>
-</div>
-<footer>
-    <div class="footer">
-      <ul class="foot_left">INFOS PRATIQUES</ul>
-      <ul class="foot_right">CONTACT</ul>
-        </div>
-  </footer>
-</body>
-</html>
+<?php require('footer.php'); ?>
