7a00d7cd466c3942a5f7ec1ce30ae4dbc69954e0
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-02-02 16:28:14 +0100

    deco

--- liste/deconnexion.php
+++ liste/deconnexion.php
@@ -15,7 +15,6 @@
             <li class="ligne right"><a href="deconnexion.php">Déconnexion</a></li>
         </ul>
     </div>
-<<<<<<< HEAD
     <div class="center">
         <h1>Maison des Ligues</h1>
         <h2>Déconnexion</h2>
@@ -24,25 +23,5 @@
         <p>Vous vous êtes bien déconnecté !</p>
         <a href="../index.php"><input type="submit" value="Retour à la page d'accueil"></a>
     </div>
-=======
-  <div class="marge">
-<br>
-<body>
-<h1>Maison des Ligues</h1>
-<h2>Déconnexion</h2>
-<center>
-<img src="../png/mdl (1).png" alt="Logo_page" title="Deconnexion" id="logo" class="centerpng"/>
-
-<p>Vous vous êtes bien déconnecté !</p>
-<form action="../index.php"><button type="submit" class="btn btn-primary">Retour à la page d'accueil</button></form>
-</center>
-</div>
-<footer>
-    <div class="footer">
-      <ul class="foot_left">INFOS PRATIQUES</ul>
-      <ul class="foot_right">CONTACT</ul>
-        </div>
-  </footer>
->>>>>>> d1f6496f42d67fc37b746e1dea2a9f655abc5e6d
 </body>
 </html>
