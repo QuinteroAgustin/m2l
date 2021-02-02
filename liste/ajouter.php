b77e3d3b797f2a2ecb390f9379e5b3b43f3fa124
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-02-02 16:24:36 +0100

    form

--- liste/ajouter.php
+++ liste/ajouter.php
@@ -7,25 +7,23 @@
     <link rel="stylesheet" href="../css/main.css">
 </head>
 <body>
-<div>
-        <ul class="navbar">
-            <li class="ligne left"><a class="tjaune" href="../index.php">Accueil</a></li>
-            <li class="ligne left"><a class="active" href="liste.php">FAQ</a></li>
-            <li class="ligne right"><a href="deconnexion.php">Déconnexion</a></li>
-        </ul>
-    </div>
+  <div>
+    <ul class="navbar">
+        <li class="ligne left"><a class="tjaune" href="../index.php">Accueil</a></li>
+        <li class="ligne left"><a class="active" href="liste.php">FAQ</a></li>
+        <li class="ligne right"><a href="deconnexion.php">Déconnexion</a></li>
+    </ul>
+  </div>
   <div class="marge">
-<br>
 <h1>Maison des Ligues</h1>
 <h2>Ajouter une question</h2>
-<form id="Ajout" action="liste.php" method="post">
-  <label for="Question" >Ecrivez votre question :</label><br>
-  <br>
-  <textarea name="question" id="question" rows="10" cols="50"></textarea></br>
-  </br>
-  <input type="submit" value="Soumettre"/>
-  <input type="submit" value="Annuler"/>
+  <form id="Ajout" action="liste.php" method="post">
+    <label for="Question" >Ecrivez votre question :</label><br>
+    <br>
+    <textarea name="question" id="question" rows="10" cols="50"></textarea></br>
+    </br>
+    <input type="submit" value="Soumettre"/>
+    <a href="ajouter.php"><input type="submit" value="Annuler"/></a>
   </form>
-  </p>
 </body>
 </html>
