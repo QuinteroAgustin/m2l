0a6fd4e172acda394a751090e57a275a29be42a9
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-02-02 16:25:16 +0100

    ajout footer

--- connexion.php
+++ connexion.php
@@ -31,6 +31,12 @@
       <input type="submit" value="Se connecter">
     </form>
   </div>
+<footer>
+    <div class="footer">
+      <ul class="foot_left">INFOS PRATIQUES</ul>
+      <ul class="foot_right">CONTACT</ul>
+        </div>
+  </footer>
 </body>
 
 </html>
--- liste/deconnexion.php
+++ liste/deconnexion.php
@@ -25,6 +25,12 @@
 <p>Vous vous êtes bien déconnecté !</p>
 <form action="../index.php"><button type="submit" class="btn btn-primary">Retour à la page d'accueil</button></form>
 </center>
-
+</div>
+<footer>
+    <div class="footer">
+      <ul class="foot_left">INFOS PRATIQUES</ul>
+      <ul class="foot_right">CONTACT</ul>
+        </div>
+  </footer>
 </body>
 </html>
