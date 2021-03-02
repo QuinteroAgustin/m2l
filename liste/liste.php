d4d8a10422ef3fba601802318741528d20bf24a0
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-03-02 17:20:37 +0100

    listes des faq

--- liste/liste.php
+++ liste/liste.php
@@ -1,4 +1,12 @@
-<?php $active=2; $title = "Liste"; require('../header.php'); ?>
+<?php $active=2; $title = "Liste"; require('../header.php'); require('../sql.php');
+    $sql="select * from faq";
+    try {
+        $sth = $dbh->query($sql);
+        $faq = $sth->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        die("Erreur lors de la requête SQL : " . $ex->getMessage());
+    }
+?>
     <h1>Liste des questions</h1>
     <table>
         <tr>
@@ -6,24 +14,22 @@
             <th>Auteur</th>
             <th>Question</th>
             <th>Réponse</th>
+            <th>Date</th>
             <th>Actions</th>
         </tr>
-        <tr>
-            <td>1</td>
-            <td>Dav</td>
-            <td>Est ce que le ballon est rond ?</td>
-            <td>Bien sûr que oui</td>
-            <td><a href="editer.php">Modifier</a></td>
-            <td><a href="supprimer.php">Supprimer</a></td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Gus</td>
-            <td>Que signifie PSG ?</td>
-            <td>Pas sûr de gagner</td>
-            <td><a href="editer.php">Modifier</a></td>
-            <td><a href="supprimer.php">Supprimer</a></td>
-        </tr>
+        <?php
+            foreach($faq as $question){
+                echo "<tr>";
+                echo "<td>".$question["id_faq"]."</td>";
+                echo "<td>".$question["id_user"]."</td>";
+                echo "<td>".$question["question"]."</td>";
+                echo "<td>".$question["reponse"]."</td>";
+                echo "<td>".$question["dat_question"]."</td>";
+                echo "<td><a href='editer.php'>Modifier</a></td>";
+                echo "<td><a href='supprimer.php'>Supprimer</a></td>";
+                echo "</tr>";
+            }
+        ?>
     </table>
     <br>
     <form action="ajouter.php"><button type="submit" class="btn btn-primary">Ajouter une question <img src="../img/add.png" alt="add"></button></form>
