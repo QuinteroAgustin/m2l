9fd48652c887f23cb2e98bb2c119f7cedda24a0b
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-03-09 16:38:35 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
     </div>
     <footer>
         <div class="footer">
-        <ul class="foot_left">INFOS PRATIQUES</ul>
+        <ul class="foot_left">
+            <li><p>INFOS PRATIQUES</p></li>
+        </ul>
         <ul class="foot_right">
-        <li class="ligne right"><a class="<?php if($active==5){echo"active";}?>" href="contact.php">CONTACT</a></li>
+            <li class="ligne right"><a class="<?php if($active==5){echo"active";}?>" href="contact.php">CONTACT</a></li>
         </ul>
         </div>
     </footer>
