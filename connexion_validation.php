6ac0d556479ec086b29176a2ac502525355e90f8
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-03-09 15:11:35 +0100

    verif connexion

--- connexion_validation.php
+++ connexion_validation.php
@@ -2,21 +2,36 @@
     $active=4; $title = "Connexion"; require('header.php'); require('sql.php');
     $pseudo=isset($_POST['pseudo']) ? $_POST['pseudo'] :  "";
     $password=isset($_POST['password']) ? $_POST['password'] :  "";
-    $sql="select * from user where pseudo=:pseudo AND mdp=:password";
-    try {
-        $sth = $dbh->prepare($sql);
-        $sth->execute(array(
-            ':pseudo' => $pseudo,
-            ':password' => $password
-        ));
-        $user = $sth->fetch(PDO::FETCH_ASSOC);
-    } catch (PDOException $ex) {
-        die("Erreur lors de la requête SQL : " . $ex->getMessage());
+    if(strlen($pseudo)>=8){
+        if(strlen($password)>=8){
+            $sql="select * from user where pseudo=:pseudo";
+            try {
+                $sth = $dbh->prepare($sql);
+                $sth->execute(array(
+                    ':pseudo' => $pseudo
+                ));
+                $user = $sth->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $ex) {
+                die("Erreur lors de la requête SQL : " . $ex->getMessage());
+            }
+            if($pseudo === $user['pseudo'] && $password===$user['mdp']){
+                unset($user["mdp"]);
+                $_SESSION['user']=$user;
+                $_SESSION['messages']=array(
+                    "connexion" => "Vous vous etes bien connecté"
+                );
+                header("Location: index.php");
+            }else{
+                $_SESSION['messages']=array("Accunt" => "Ces identifiants sont incorrecte");
+                header("Location: connexion.php");
+            }
+            
+        }else{
+            $_SESSION['messages']=array("Password" => "Vous avez rentrez un mot de passe trop court");
+            header("Location: connexion.php");
+        }
+    }else{
+        $_SESSION['messages']=array("Pseudo" => "Vous avez rentrez un pseudo trop court");
+        header("Location: connexion.php");
     }
-    unset($user["mdp"]);
-    $_SESSION['user']=$user;
-    $_SESSION['messages']=array(
-        "connexion" => "Vous vous etes bien connecté"
-    );
-    header("Location: index.php");
 ?>
