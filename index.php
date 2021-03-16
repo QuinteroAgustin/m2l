432db63711562ecc176a735cb3248f0471b8921f
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-03-16 14:47:43 +0100

    supp

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php $active=1; $title = "Accueil"; require('header.php'); ?>
     <br>
+    <body>
     <img src="img/logo.png" alt="logo">
     <br>
         <h1>Maison de ligue</h1>
         <p>Ll456416546orem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in illo dolorum eveniet unde explicabo aut incidunt deserunt nemo, qui nesciunt officiis modi, sint laboriosam numquam ut dolorem sit voluptas.lore Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam qui nulla natus reprehenderit doloribus nihil corporis architecto est voluptatem expedita mollitia recusandae voluptates asperiores enim, voluptatum inventore neque voluptatibus. Autem?</p>
     </div>
+    <body>
 <?php require('footer.php'); ?>
--- liste/supprimer.php
+++ liste/supprimer.php
@@ -1,15 +1,30 @@
-<?php $active=2; $title = "Supprimer"; require('../header.php'); ?>
+<?php $active=2; $title = "Supprimer"; require('../header.php'); require('../sql.php') ?>
+<?php
+if(isset($_GET['id'])){
+    $id = isset($_GET['id'])?$_GET['id']:null;
+    $sql = "SELECT question, reponse FROM faq WHERE id_faq=:id";
+    try {
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array(
+    ':id' => $id
+    ));
+    $question=$sth->fetch(PDO::FETCH_ASSOC);
+    } catch ( PDOException $ex) {
+    die("Erreur lors de la requête SQL : ".$ex->getMessage());
+    }
+}
+
+?>
+  
   <h1>Supprimer une question</h1>
-  <h3>Question</h3>
-  <form action="liste.php" method="POST">
-    <textarea name="question" id="question" rows="10" cols="50" >Est ce que le ballon est rond ?</textarea>
+  <form id="supprimer" action="supprimer_validation.php" method="post">
+    <h3>Question</h3>
+    <textarea name="question" id="question" rows="10" cols="50" disabled="disabled"><?= $question['question'];?></textarea>
     <h3>Réponse</h3>
-    <textarea name="reponse" id="reponse" rows="10" cols="50" >Bien sûr que oui</textarea>
-    <br>
-    <br>
-    <input type="submit" value="Supprimer">
+    <textarea name="reponse" id="reponse" rows="10" cols="50" disabled="disabled" ><?= $question['reponse'];?></textarea>
+    <br></br>
+    <input type="submit" value="Confirmer"/>
   </form>
-  </br>
-  <a href="liste.php"><input type="submit" value="Annuler"></a>
   <br>
-<?php require('../footer.php'); ?>                
+  <a href="liste.php"><input type="submit" value="Retour"></a>
+<?php require('../footer.php'); ?>
