b0ee5acbc1213db69fc30fc34034b5029fcfd33c
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-03-16 13:58:19 +0100

    verif si user est deja dans la base

--- inscription_validation.php
+++ inscription_validation.php
@@ -9,27 +9,42 @@
     if ($submit) {
         if(strlen($pseudo)>=8){
             if(strlen($password)>=8){
-                if($_POST['password']==$_POST['password2']) {
-                    $password=password_hash($password, PASSWORD_BCRYPT);
-                    $sql = "INSERT INTO user (pseudo, mdp, mail, id_usertype, id_ligue) VALUES (:pseudo, :password, :mail, :id_usertype,:id_ligue)";
-                    try {
+                if($password==$password2) {
+                    $sql="SELECT pseudo, mail FROM user WHERE pseudo LIKE :pseudo OR mail lIKE :email";
+                    try{
                         $sth = $dbh->prepare($sql);
                         $sth->execute(array(
-                            ':pseudo' => $pseudo,
-                            ':password' => $password,
-                            ':mail' => $email,
-                            ':id_usertype' => 1,
-                            ':id_ligue' => $ligue
+                            ":pseudo" => $pseudo,
+                            ":email" => $email
                         ));
                         $user = $sth->fetch(PDO::FETCH_ASSOC);
-                    } catch (PDOException $ex) {
+                    }catch (PDOException $ex){
                         die("Erreur lors de la requête SQL : " . $ex->getMessage());
                     }
+                    if(!($user['mail'] == $email || $user['pseudo']==$pseudo)){
+                        $password=password_hash($password, PASSWORD_BCRYPT);
+                        $sql = "INSERT INTO user (pseudo, mdp, mail, id_usertype, id_ligue) VALUES (:pseudo, :password, :mail, :id_usertype,:id_ligue)";
+                        try {
+                            $sth = $dbh->prepare($sql);
+                            $sth->execute(array(
+                                ':pseudo' => $pseudo,
+                                ':password' => $password,
+                                ':mail' => $email,
+                                ':id_usertype' => 1,
+                                ':id_ligue' => $ligue
+                            ));
+                        } catch (PDOException $ex) {
+                            die("Erreur lors de la requête SQL : " . $ex->getMessage());
+                        }
 
-                    $_SESSION['messages']=array(
-                        "inscription" => "Vous vous etes bien inscrit"
-                    );
-                    header("Location: connexion.php");
+                        $_SESSION['messages']=array(
+                            "inscription" => "Vous vous êtes bien inscrit !"
+                        );
+                        header("Location: connexion.php");
+                    }else{
+                        $_SESSION['messages']=array("Password" => "Cette utilisateur ou email existe déjà.");
+                        header("Location: inscription.php");
+                    }
                 }else{
                     $_SESSION['messages']=array("Password" => "Les mots de passe ne sont pas identiques");
                     header("Location: inscription.php");
