950ae25e84c164414efdd6d885a4ff94fed4e32c
DavidPeyrard <david.peyrard@limayrac.fr>
2021-03-23 14:48:24 +0100

    coms

--- connexion_validation.php
+++ connexion_validation.php
@@ -1,32 +1,42 @@
 <?php 
     $active=4; $title = "Connexion"; require('header.php'); require('sql.php');
+    //Le pseudo saisi par l'user va dans la variable $pseudo
     $pseudo=isset($_POST['pseudo']) ? $_POST['pseudo'] :  "";
+    //Le mdp saisi par l'user va dans la variable $password
     $password=isset($_POST['password']) ? $_POST['password'] :  "";
+    //Si pseudo sup à 8 carac.
     if(strlen($pseudo)>=8){
+        //Si mdp sup à 8 carac.
         if(strlen($password)>=8){
+            //On rentre la requête sql dans une variable
             $sql="select * from user where pseudo=:pseudo";
+            //Lecture du pseudo dans la BDD 
             try {
                 $sth = $dbh->prepare($sql);
                 $sth->execute(array(
                     ':pseudo' => $pseudo
                 ));
                 $user = $sth->fetch(PDO::FETCH_ASSOC);
-            } catch (PDOException $ex) {
+            } //Gestion d'erreurs
+            catch (PDOException $ex) {
                 die("Erreur lors de la requête SQL : " . $ex->getMessage());
             }
+            //Le mot de passe est de nouveau crypté lors de la saisie
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            //pour le comparer avec celui enregistré lors de l'inscription
             if($pseudo === $user['pseudo'] && password_verify($password,$hashed_password)){
                 unset($user["mdp"]);
                 $_SESSION['user']=$user;
                 $_SESSION['messages']=array(
                     "connexion" => "Vous vous êtes bien connecté"
                 );
+                //Redirige vers l'accueil si connexion réussie
                 header("Location: index.php");
+            //Cas où la connexion échoue
             }else{
                 $_SESSION['messages']=array("Account" => "Ces identifiants sont incorrects");
                 header("Location: connexion.php");
             }
-            
         }else{
             $_SESSION['messages']=array("Password" => "Vous avez rentré un mot de passe trop court");
             header("Location: connexion.php");
--- inscription_validation.php
+++ inscription_validation.php
@@ -1,16 +1,23 @@
 <?php 
     $active=3; $title = "Inscription"; require('header.php'); require('sql.php');
+    //Le pseudo saisi par l'user va dans la variable $pseudo
     $pseudo=isset($_POST['pseudo']) ? $_POST['pseudo'] :  "";
+    //Le mail saisi par l'user va dans la variable $email
     $email=isset($_POST['email']) ? $_POST['email'] :  "";
     $password=isset($_POST['password']) ? $_POST['password'] :  "";
     $password2=isset($_POST['password2']) ? $_POST['password2'] :  "";
     $ligue=isset($_POST['ligue']) ? $_POST['ligue'] : '';
     $submit=isset($_POST['submit']);
+    //Si cliqué sur submit
     if ($submit) {
+        //Si pseudo sup à 8 carac.
         if(strlen($pseudo)>=8){
+            //Si mdp sup à 8 carac.
             if(strlen($password)>=8){
+                //Si 2 mdp identiques
                 if($password==$password2) {
                     $sql="SELECT pseudo, mail FROM user WHERE pseudo LIKE :pseudo OR mail lIKE :email";
+                    //Lecture du pseudo et du mail dans la BDD pour comparer si ceux-ci existent déjà ou non
                     try{
                         $sth = $dbh->prepare($sql);
                         $sth->execute(array(
@@ -21,9 +28,13 @@
                     }catch (PDOException $ex){
                         die("Erreur lors de la requête SQL : " . $ex->getMessage());
                     }
+                    //Si le mail ou le pseudo n'existe pas déjà alors on peut s'inscrire
                     if(!($user['mail'] == $email || $user['pseudo']==$pseudo)){
+                        //On crypte le mdp
                         $password=password_hash($password, PASSWORD_BCRYPT);
+                        //On insère les champs saisis dans la BDD avec la requête SQL
                         $sql = "INSERT INTO user (pseudo, mdp, mail, id_usertype, id_ligue) VALUES (:pseudo, :password, :mail, :id_usertype,:id_ligue)";
+                        //Insertion des infos de l'user dans la BDD
                         try {
                             $sth = $dbh->prepare($sql);
                             $sth->execute(array(
@@ -33,7 +44,8 @@
                                 ':id_usertype' => 1,
                                 ':id_ligue' => $ligue
                             ));
-                        } catch (PDOException $ex) {
+                        } //Gestion d'erreurs
+                        catch (PDOException $ex) {
                             die("Erreur lors de la requête SQL : " . $ex->getMessage());
                         }
 
@@ -41,7 +53,8 @@
                             "inscription" => "Vous vous êtes bien inscrit !"
                         );
                         header("Location: connexion.php");
-                    }else{
+                    } //Cas où la connexion échoue
+                    else{
                         $_SESSION['messages']=array("Password" => "Cet utilisateur ou email existe déjà.");
                         header("Location: inscription.php");
                     }
