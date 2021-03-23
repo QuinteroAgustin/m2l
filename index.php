1cd1bf6713315f5efe845771d5777986ce99256c
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-03-23 16:27:48 +0100

    modif

--- index.php
+++ index.php
@@ -4,7 +4,23 @@
     <img src="img/logo.png" alt="logo">
     <br>
         <h1>Maison de ligue</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in illo dolorum eveniet unde explicabo aut incidunt deserunt nemo, qui nesciunt officiis modi, sint laboriosam numquam ut dolorem sit voluptas.lore Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam qui nulla natus reprehenderit doloribus nihil corporis architecto est voluptatem expedita mollitia recusandae voluptates asperiores enim, voluptatum inventore neque voluptatibus. Autem?</p>
+        <h2>Qui sommes nous ?</h2>
+        <p>La maison des ligues Lorraine est une structure financée par le Conseil Régional. Elle a pour mission de fournir des espaces et des services aux différentes ligues sportives régionales et à d’autres structures hébergées</p>
+        <br>
+        <h2>Que proposons-nous ?</h2>
+        <h3><strong>Des missions</strong></h3>
+        <h4>Le personnel de la M2L et leurs missions</h4>
+        <p>Les personnes qui travaillent ici sont de différentes catégories:</p>
+        <li>Les «professionnels»
+        <ul>
+        </ul>
+        </li>
+        <h3>Des services</h3>
+        <p></p>
+        <h3>Des financements</h3>
+        <p></p>
+
+
     </div>
     <body>
 <?php require('footer.php'); ?>
