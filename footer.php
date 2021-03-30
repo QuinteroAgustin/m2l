e6ec7779d305ec392e879d987759af7831c98198
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-03-30 16:03:35 +0200

    tedt

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     </div>
     <div class="footer">
         <ul>
-            <li class="ligne left"><p>INFOS PRATIQUES</p></li>
+            <li class="ligne left"><a>INFOS PRATIQUES</a></li>
             <li class="ligne right"><a class="<?php if($active==5){echo"active";}?>" href="/m2l/contact.php">CONTACT</a></li>
         </ul>
 </body>
