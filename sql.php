cc8bc2e577d44dba3a990d4c8ee69e0e100697ed
david <david.peyrard@limayrac.fr>
2021-11-18 16:18:44 +0100

    ok

--- sql.php
+++ sql.php
@@ -1,8 +1,8 @@
 <?php
   //Permet la connexion à la BDD avec les paramètres
   $dsn='mysql:host=127.0.0.1;dbname=m2l';
-  $username="maison2ligue";
-  $password="Maisondeuxligues";
+  $username="root";
+  $password="";
   //Crée une connexion PDO
   try {
       $dbh = new PDO($dsn, $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
